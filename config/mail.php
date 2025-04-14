26ed8c1d1150f235396dd69bd20ac85cd11498ea
Mahmud Numan <mahmudnuman@gmail.com>
2025-04-14 11:48:19 +0600

    mailtrap integrated for email notification

--- config/mail.php
+++ config/mail.php
@@ -36,17 +36,16 @@ return [
     */
 
     'mailers' => [
-
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+            'auth_mode' => null,
         ],
-
         'ses' => [
             'transport' => 'ses',
         ],
@@ -107,10 +106,8 @@ return [
     | used globally for all emails that are sent by your application.
     |
     */
-
-    'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
-
+'from' => [
+    'address' => env('MAIL_FROM_ADDRESS', 'no-reply@example.com'),
+    'name' => env('MAIL_FROM_NAME', 'Job Portal'),
+],
 ];
--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,19 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ApplicationController;
 
+
+Route::get('/test-mail', function () {
+    Mail::raw('This is a Mailtrap test email from Job Portal!', function ($message) {
+        $message->to('mahmudnuman@gmail.com')->subject('Test Email');
+    });
+
+    return 'Test email sent!';
+});
+
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::get('/listings', [ListingController::class, 'index']); // List all jobs
 
